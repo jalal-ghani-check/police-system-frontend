f38988bde3f855f891b857f29b4b709fbd401abc
sarmad92 <sarmad@foomotion.io>
2021-08-04 00:32:39 +0500

    -

---
+++ pages/messages.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>hello</div>
+</template>
+
+// <script>
+// export default {
+//   mounted() {
+//     this.socket = this.$nuxtSocket({
+//       channel: ''
+//     })
+//     /* Listen for events: */
+//     this.socket
+//     .on('someEvent', (msg, cb) => {
+//       /* Handle event */
+//     })
+//   },
+//   methods: {
+//     method1() {
+//       /* Emit events */
+//       this.socket.emit('method1', {
+//         hello: 'world' 
+//       }, (resp) => {
+//         /* Handle response, if any */
+//       })
+//     }
+//   }
+// }
+// </script>
