c0f68fc2b7a45236477c2ebf4c976981a4eda3c9
Jalal Ghani <jalalghani@Jalals-MacBook-Pro.local>
2021-08-03 20:36:52 +0500

    deploy change

---
+++ dist/_nuxt/2525ec7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{682:function(e,n,t){"use strict";t.r(n);var r={middleware:["redirect"]},l=t(28),component=Object(l.a)(r,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("\n  Landing Page\n")])}),[],!1,null,null,null);n.default=component.exports}}]);
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,11 @@
-export default {
+export default {    
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target : "static",
+  ssr : false,
+  generate: {
+    fallback: true
+  },
+  
   head: {
     titleTemplate: '%s - Police System',
     title: 'Frontend',
