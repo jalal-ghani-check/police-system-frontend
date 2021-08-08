8ceb568c99725c0b62531088adbff6d3178fa81f
sarmad92 <sarmad@foomotion.io>
2021-08-08 12:15:39 +0500

    -

--- components/warrants/warrantItem.vue
+++ components/warrants/warrantItem.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="warrants-item report-card-frontend" @click="detailWarrant(warrant)" :class="{active: warrant.status === 'approved'}">
-      <div class="tag-no">#{{ warrant.warrant_id }}</div>
-      <div class="wanted-name">
-          <h3>Wanted</h3>
-          <h4>{{ warrant.profile_full_name }}</h4>
-          <h5>By: {{ warrant.user_full_name }}</h5>
+  <div class="warrants-item report-card-frontend" :class="{active: warrant.status === 'approved'}">
+       <div @click="detailWarrant(warrant)" class="warrant-basic-info-wrapper"> 
+        <div class="tag-no">#{{ warrant.warrant_id }}</div>
+        <div class="wanted-name">
+            <h3>Wanted</h3>
+            <h4>{{ warrant.profile_full_name }}</h4>
+            <h5>By: {{ warrant.user_full_name }}</h5>
+        </div>
+        <div class="created-by">Created: {{ warrant.created_at }}</div>
+        <div v-if="warrant.status === 'served'" class="tag">Served Warrant</div>
+        <div v-if="warrant.status === 'pending'" class="tag">Pending Warrant</div>
+        <div v-else-if="warrant.status === 'approved'" class="tag">Active Warrant</div>
+        <div v-else-if="warrant.status === 'rejected'" class="tag">Rejected Warrant</div>
       </div>
-      <div class="created-by">Created: {{ warrant.created_at }}</div>
-      <div v-if="warrant.status === 'served'" class="tag">Served Warrant</div>
-      <div v-if="warrant.status === 'pending'" class="tag">Pending Warrant</div>
-      <div v-else-if="warrant.status === 'approved'" class="tag">Active Warrant</div>
-      <div v-else-if="warrant.status === 'rejected'" class="tag">Rejected Warrant</div>
-
       <div class="warrants-item-footer d-grid gap-2">
           <button v-if="(warrant.status === 'pending') && isAllowedToApproveWarrants" @click="acceptWarrant(warrant.enc_warrant_id)" class="btn btn-outline-success">Accept</button>
           <button v-if="(warrant.status === 'pending') && isAllowedToDeleteWarrants"   @click="rejectWarrant(warrant.enc_warrant_id)" class="btn btn-outline-danger">Reject</button>
@@ -77,3 +78,8 @@ export default {
 
 
 
+<style scoped>
+.warrant-basic-info-wrapper:hover {
+  cursor: pointer;
+}
+</style>
